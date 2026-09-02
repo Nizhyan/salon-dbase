1b429f99fab99606f9bbfe4e49d9055c86045456
Nizhyan <219461998+Nizhyan@users.noreply.github.com>
2026-09-03 00:24:52 +0300

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -17,7 +17,8 @@ display_services() {
 display_services
 
 echo
-read -p "What would you like to do? " SERVICE_ID_SELECTED
+echo "What would you like to do?"
+read SERVICE_ID_SELECTED
 
 while [[ -z $($PSQL "SELECT service_id FROM services WHERE service_id = $SERVICE_ID_SELECTED") ]]
 do
@@ -25,22 +26,26 @@ do
   echo
   display_services
   echo
-  read -p "What would you like to do? " SERVICE_ID_SELECTED
+  echo "What would you like to do?"
+  read SERVICE_ID_SELECTED
 done
 
-read -p "What's your phone number? " CUSTOMER_PHONE
+echo "What's your phone number?"
+read CUSTOMER_PHONE
 
 CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'" | head -1 | xargs)
 
 if [[ -z $CUSTOMER_NAME ]]
 then
-  read -p "I don't have a record for that phone number, what's your name? " CUSTOMER_NAME
+  echo "I don't have a record for that phone number, what's your name?"
+  read CUSTOMER_NAME
   CUSTOMER_ID=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME') RETURNING customer_id" | head -1 | xargs)
 else
   CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'" | head -1 | xargs)
 fi
 
-read -p "What time would you like your service, $CUSTOMER_NAME? " SERVICE_TIME
+echo "What time would you like your service, $CUSTOMER_NAME?"
+read SERVICE_TIME
 
 INSERT_APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
 
